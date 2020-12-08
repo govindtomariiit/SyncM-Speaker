5169b1902bcda840ddade0de6745b61d1fa185a5
codergarv <codergarv22@gmail.com>
2020-12-08 19:06:24 +0530

    cleaned code with message functionality

--- public/script.js
+++ public/script.js
@@ -1,18 +1,10 @@
 console.log('SCRIPT WORKING')
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 const socket = io('http://localhost:3000')
 const name = prompt('What is your name?')
 socket.emit('new-user', roomid, name);
-const listofuser = document.getElementById('listofuser');
-
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
-
-function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  listofuser.append(messageElement)
-}
 
 function handleplaysound(){
   console.log("clicked playsound");
@@ -42,3 +34,43 @@ socket.on('pauseonall',msg =>{
     x.currentTime=0;
     x.pause();
 })
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
+});
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach(user=>{
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+ }
--- utils/users.js
+++ utils/users.js
@@ -3,14 +3,13 @@ const users = [];
 // Join user to chat
 function userJoin(id, username, room) {
   const user = { id, username, room };
-
   users.push(user);
-
   return user;
 }
 
 // Get current user
 function getCurrentUser(id) {
+    console.log("get user " + users);
   return users.find(user => user.id === id);
 }
 
